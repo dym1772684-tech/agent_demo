cbf6055961a9a1b860b4cd43da2c853e0a2f2421
dym2 <1772684>
2026-05-01 13:01:31 +0800

    new change

---
+++ agent.py
@@ -0,0 +1,15 @@
+class Restaurant:
+   def __init__(self,restaurant_name,cuisine_type):
+      self.restaurant_name=restaurant_name
+      self.cuisine_type=cuisine_type
+   def describe_restaurant(self):
+        print(self.restaurant_name)
+        print(self.cuisine_type)
+
+   def open_restaurant(self):
+        print("正在营业")
+one =Restaurant("dym","name")
+one.describe_restaurant()
+one.open_restaurant()
+
+   
